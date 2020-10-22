ae9342c8b702213376f17137be827efe1bc01df5
Maya Malik <maya28892@gmail.com>
2020-10-22 18:42:00 +0300

    some more design

--- js/controller.js
+++ js/controller.js
@@ -9,6 +9,7 @@ function onInit() {
     gCtx.fillStyle = 'white'
     gCtx.strokeStyle = 'black'
     renderImgs();
+    renderShare();
 }
 
 function resizeCanvas() {
@@ -147,3 +148,34 @@ function onGalleryClicked() {
 //     // console.log(gCanvas.width);
 //     return (gCanvas.width / 2);
 // }
+
+function renderShare() {
+    // ev.preventDefault();
+    const elForm = document.querySelector('.download-tools form');
+    console.log(elForm)
+    document.getElementById('imgData').value = gCanvas.toDataURL("image/jpeg");
+
+    function onSuccess(uploadedImgUrl) {
+        uploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        document.querySelector('.share-container').innerHTML = `
+        <a class="btn-share" href="https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
+           Share   
+        </a>`
+    }
+    doUploadImg(elForm, onSuccess);
+}
+
+function doUploadImg(elForm, onSuccess) {
+    var formData = new FormData(elForm);
+    fetch('http://ca-upload.com/here/upload.php', {
+            method: 'POST',
+            body: formData
+        })
+        .then(function(res) {
+            return res.text()
+        })
+        .then(onSuccess)
+        .catch(function(err) {
+            console.error(err)
+        })
+}
